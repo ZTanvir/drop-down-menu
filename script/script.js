021f87f7c61a520264e4f56bdb4d20d03545b992
ztanvir <zahirulopel@gmail.com>
2023-04-10 21:21:44 +0600

    Change image onclick

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
 const dropDownMenu = document.querySelector(".drop-down__menu");
 const dropDownSubMenu = document.querySelector(".drop-down__submenu");
+const dropDownImg = document.querySelector(".drop-down__menu-link");
+
 
 function dropDown(clickElement,addClassElement,cssClass){
     clickElement.addEventListener("click",(e)=>{
         e.preventDefault();
         addClassElement.classList.toggle(cssClass);
+        let imageSrc = dropDownImg.src;
+        if(imageSrc.includes("expandmore")){
+            dropDownImg.src = "/drop-down-menu/images/expandless.svg";
+        }else{
+            dropDownImg.src = "/drop-down-menu/images/expandmore.svg";
+        }
     })
 }
 dropDown(dropDownMenu,dropDownSubMenu,"hide-menu");
